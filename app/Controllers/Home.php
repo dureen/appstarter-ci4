36e23ef023f1a269f2cedc4c055cc18877c61aa1
dureen <dpdevproject@gmail.com>
2023-02-12 13:54:34 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,8 @@ class Home extends BaseController
 {
     public function index()
     {
+        // return view('welcome_message');
+
         // if($session->get('name'))
         return view('template/header')
             . view('template/navigation')
